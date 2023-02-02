ac257da7b38de43ea5a98101434d5049a561d31a
Ali <avohra@elevatesaas.com>
2023-02-02 17:34:40 +0530

    change introduced

--- TestApp/Controllers/WeatherForecastController.cs
+++ TestApp/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace TestApp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Unbearable"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
